4dafc6b1d468268f2e59ffa29f136cfb0ed4b46d
CaioD13 <115421030+CaioD13@users.noreply.github.com>
2026-03-18 15:22:30 -0300

    Delete Sistemas directory

--- Sistemas/PGB/Iniciar.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Sistema de Protocolos - P1
-echo Iniciando o Servidor de Protocolos (Porta 8080)...
-python app.py
-pause
--- Sistemas/SGL/iniciar.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Sistema SGL - P4
-echo Iniciando o Servidor de Logistica (Porta 8084)...
-python app.py
-pause
